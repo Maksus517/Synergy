626ab64792f49b038f72af272887ec21e6ae1a6b
Maksus517 <maksim517@mail.ru>
2024-06-29 16:04:05 +0300

    added lesson_5_task_3.py

--- lesson_5_task_2.py
+++ lesson_5_task_2.py
@@ -1,8 +1,13 @@
-letters: list = ['a', 'e', 'i', 'o', 'u']
-string: str = input("Введите слово: ").lower()
-result: int = 0
-for letter in letters:
-    if letter in string:
-        result += 1
+def main():
+    letters: list = ['a', 'e', 'i', 'o', 'u']
+    string: str = input("Введите слово: ").lower()
+    result: int = 0
+    for letter in letters:
+        if letter in string:
+            result += 1
 
-print(result) if result else print(False)
+    print(result) if result else print(False)
+
+
+if __name__ == '__main__':
+    main()
--- lesson_5_task_3.py
+++ lesson_5_task_3.py
@@ -1,13 +1,21 @@
-x, a, b = float(input("введите минимальную сумму: ")), \
-    float(input("введите сумму Майкла: ")), float(input("введите сумму Ивана: "))
+def main():
+    x, a, b = float(input("Введите минимальную сумму: ")), \
+        float(input("Введите сумму Майкла: ")), float(input("Введите сумму Ивана: "))
 
-if a >= x and b >= x:
-    print(2)
-elif a >= x >= b:
-    print("Mike")
-elif b >= x >= a:
-    print("Ivan")
-elif a + b >= x:
-    print(1)
-else:
-    print(0)
+    if a >= x and b >= x:
+        print(2)
+    elif a >= x >= b:
+        print("Mike")
+    elif b >= x >= a:
+        print("Ivan")
+    elif a + b >= x:
+        print(1)
+    else:
+        print(0)
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except ValueError:
+        print("Неверный формат ввода")
