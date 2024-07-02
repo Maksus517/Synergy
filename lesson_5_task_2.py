41bd86322868a4d42cb4ab1d27ba16def60b4235
Maksus517 <maksim517@mail.ru>
2024-07-03 00:18:18 +0300

    changed lesson_5_task_2.py

--- lesson_5_task_2.py
+++ lesson_5_task_2.py
@@ -1,12 +1,19 @@
 def main():
-    letters: list = ['a', 'e', 'i', 'o', 'u']
+    letters: dict[str, int] = {'a': 0, 'e': 0, 'i': 0, 'o': 0, 'u': 0}
     string: str = input("Введите слово: ").lower()
-    result: int = 0
-    for letter in letters:
-        if letter in string:
-            result += 1
+    vowels: int = 0
+    consonants: int = 0
+    for letter in string:
+        if letter in letters.keys():
+            letters[letter] += 1
+            vowels += 1
+        else:
+            consonants += 1
+    for k, v in letters.items():
+        print(f"Буква {k} - {v}") if v else print(False)
+    print(f"Гласных - {vowels}")
+    print(f"Согласных - {consonants}")
 
-    print(result) if result else print(False)
 
 
 if __name__ == '__main__':
