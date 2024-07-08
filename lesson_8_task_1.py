32bf35ef8c9ad0e02a2ed17f3657b31a0feb0eb1
Maksus517 <maksim517@mail.ru>
2024-07-08 12:04:11 +0300

    added lesson_8_task_1.py

--- lesson_8_task_1.py
+++ lesson_8_task_1.py
@@ -1,9 +1,20 @@
 def main():
     res = []
-    for _ in range(int(input())):
-        res.append(int(input()))
-    res.reverse()
-    print(*res)
+    for _ in range(get_checked_num()):
+        res.append(get_checked_num())
+    print(*res[::-1])
+
+
+def get_checked_num() -> int:
+    while True:
+        try:
+            num: int = int(input("Введите число от 1 до 10000: "))
+            if 1 <= num <= 10000:
+                return num
+            print("Попробуйте еще раз")
+        except ValueError:
+            print("Неверный формат ввода, попробуйте еще раз")
+            continue
 
 
 if __name__ == '__main__':
