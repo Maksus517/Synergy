9ca17870758a7d872c73b674ce8e37cacc8425c9
Maksus517 <maksim517@mail.ru>
2024-07-12 17:26:48 +0300

    added lesson_9_task_2.py

--- lesson_9_task_2.py
+++ lesson_9_task_2.py
@@ -1,5 +1,7 @@
 def main():
-    print(len(set(input().split()) & set(input().split())))
+    row1: set = set(filter(None, input("Введите первую строку чисел: ").split()))
+    row2: set = set(filter(None, input("Введите вторую строку чисел: ").split()))
+    print(len(row1 & row2))
 
 
 if __name__ == '__main__':
